266185e6f82e571d432efef2fcd1d8cc5a874ba8
maia grotepass <maiatoday@gmail.com>
2021-06-06 21:26:34 +0200

    Updates and make the pie animate

--- app/src/main/java/net/maiatoday/composesampler/ui/components/PieStatus.kt
+++ app/src/main/java/net/maiatoday/composesampler/ui/components/PieStatus.kt
@@ -2,12 +2,14 @@ package net.maiatoday.composesampler.ui.components
 
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme.colors
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
@@ -29,6 +31,14 @@ fun PieStatus(
     modifier: Modifier = Modifier,
     pieData: PieData
 ) {
+    var animationPlayed by remember { mutableStateOf(false) }
+    val currentPercentage = animateFloatAsState(
+        targetValue = if (animationPlayed) pieData.percentage else 0f,
+        animationSpec = tween(3000)
+    )
+    LaunchedEffect(key1 = true) {
+        animationPlayed = true
+    }
     Canvas(
         modifier = modifier
     ) {
@@ -37,14 +47,14 @@ fun PieStatus(
         drawCircle(
             color = pieData.foreground,
             center = Offset(x = canvasWidth / 2, y = canvasHeight / 2),
-            radius = canvasWidth/2 - pieData.strokeWidth.toPx(),
+            radius = canvasWidth / 2 - pieData.strokeWidth.toPx(),
             style = Stroke(width = pieData.strokeWidth.toPx())
         )
         val arcPadding = pieData.strokeWidth.toPx() * 2
         drawArc(
             color = pieData.foreground,
             startAngle = -90f,
-            sweepAngle = pieData.percentage * 360,
+            sweepAngle = currentPercentage.value * 360,
             useCenter = true,
             topLeft = Offset(arcPadding, arcPadding),
             size = Size(size.width - (arcPadding * 2f), size.height - (arcPadding * 2f))
@@ -55,11 +65,13 @@ fun PieStatus(
 @Preview(
     name = "PieStatus Night Preview",
     showBackground = true,
-    uiMode = UI_MODE_NIGHT_YES)
+    uiMode = UI_MODE_NIGHT_YES
+)
 @Preview(
     name = "PieStatus Day Preview",
     showBackground = true,
-    uiMode = UI_MODE_NIGHT_NO)
+    uiMode = UI_MODE_NIGHT_NO
+)
 @Composable
 fun DefaultPreview() {
     ComposeSamplerTheme {
@@ -69,7 +81,8 @@ fun DefaultPreview() {
                 .padding(8.dp),
             pieData = PieData(
                 foreground = colors.onSurface,
-                percentage = 0.62f)
+                percentage = 0.62f
+            )
         )
     }
 }
